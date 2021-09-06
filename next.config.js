0a4beb4b55b5aa3c7e69ad5e64becdd5905c3ce9
Dayo Adebanjo <dahyor@outlook.com>
2021-09-06 18:04:47 +0100

    Removed Secret

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
+module.exports = (phase) => {
+  if ( phase === PHASE_DEVELOPMENT_SERVER){
+    return {
+      reactStrictMode: true,
+      env: {
+        mongoUri: 'mongodb+srv://test:node@node.lzn3y.mongodb.net/NextJs-dev?retryWrites=true&w=majority'
+      }
+    }
+  }
+
+  return {
+    reactStrictMode: true,
+    env: {
+      mongoUri: 'mongodb+srv://test:node@node.lzn3y.mongodb.net/NextJs?retryWrites=true&w=majority'
+    }
+  }
 }
